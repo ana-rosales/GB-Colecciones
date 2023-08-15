389393fdd35cc1e388a60c4ec34aff7eb58dab7d
Ana Rosales <anapaula161103@gmail.com>
2023-08-14 19:27:56 -0600

    Ejercicio 01 Guia: Completado

---
+++ GUIACOLECCIONES/src/guiacolecciones/main.java
@@ -0,0 +1,28 @@
+
+package guiacolecciones;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Ana Paula
+ */
+public class main {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Ejercicio 01:
+         * Corrige el siguiente código: Array<int> listado = new ArrayList;
+         * TreeSet<String> = TreeSet(); HashMap<Integer> personas = new
+         * HashMap<>;
+         */
+        ArrayList<Integer> listado = new ArrayList<>();
+        TreeSet<String> setordenado = new TreeSet<>();
+        HashMap<Integer,String> personas = new HashMap<>();
+        
+    }
+    
+}
